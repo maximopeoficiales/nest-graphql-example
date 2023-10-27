13743cbdf372dadae70d4c416e1ea6a7f8955099
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2023-10-26 22:49:54 -0500

    ADD: fin de seccion 8 - video 117

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ItemsModule } from 'src/items/items.module';
+import { UsersModule } from 'src/users/users.module';
 import { SeedResolver } from './seed.resolver';
 import { SeedService } from './seed.service';
 
 @Module({
   providers: [SeedResolver, SeedService],
-  imports: [ConfigModule],
+  imports: [ConfigModule, UsersModule, ItemsModule],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from 'src/items/entities/item.entity';
+import { ItemsService } from 'src/items/items.service';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { Repository } from 'typeorm';
+import { SEED_ITEMS, SEED_USERS } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(Item) private readonly itemRepository: Repository<Item>,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly userService: UsersService,
+    private readonly itemsService: ItemsService,
+  ) {
     this.isProd = this.configService.get('STATE') === 'prod';
   }
 
@@ -14,8 +27,35 @@ export class SeedService {
     }
 
     // Limpiar la base de datos
+    await this.deleteDatabase();
     // Crear Usuarios
+    const user = await this.loadUsers();
     // Crear Items
+    await this.loadItems(user);
     return true;
+  }
+
+  async deleteDatabase() {
+    // borrar items
+    await this.itemRepository.createQueryBuilder().delete().where({}).execute();
+    // borrar usuarios
+    await this.userRepository.createQueryBuilder().delete().where({}).execute();
+  }
+
+  async loadUsers(): Promise<User> {
+    const users = [];
+    for (const user of SEED_USERS) {
+      users.push(await this.userService.create(user));
+    }
+    return users[0];
+  }
+
+  async loadItems(user: User): Promise<void> {
+    const itemsPromises = [];
+    for (const item of SEED_ITEMS) {
+      itemsPromises.push(this.itemsService.create(item, user));
+    }
+
+    await Promise.all(itemsPromises);
   }
 }
